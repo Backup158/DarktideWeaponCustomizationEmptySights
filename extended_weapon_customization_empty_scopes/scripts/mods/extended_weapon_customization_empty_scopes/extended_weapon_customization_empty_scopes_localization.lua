42fa5021595789cd97c69859f7e84b41f14cec00
LackupL58 <beekoop158+gh@gmail.com>
2025-10-02 00:47:49 -0600

    local: settings and separator name

--- extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes_localization.lua
+++ extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes_localization.lua
@@ -1,5 +1,21 @@
+local mod = get_mod("extended_weapon_customization_empty_scopes")
+
+-- Localizations for the base mod to use
+mod:add_global_localize_strings({
+	-- Mod Name for the separators
+    loc_extended_weapon_customization_empty_scopes = {
+        en = "Empty Scopes/Sights",
+    },
+})
+
 return {
 	mod_description = {
-		en = "%%description",
+		en = "Adds optics without reticles, for use with Crosshair Remap",
+	},
+	debug_mode = {
+		en = "Debug Mode",
+	},
+	debug_mode_description = {
+		en = "Verbose printing in the console log",
 	},
 }
