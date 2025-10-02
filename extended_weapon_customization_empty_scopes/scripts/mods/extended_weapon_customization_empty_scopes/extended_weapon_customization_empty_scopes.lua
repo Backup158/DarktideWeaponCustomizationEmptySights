450ca50ba501b2c2f6b7935ec89d9b0f3efd89b7
LackupL58 <beekoop158+gh@gmail.com>
2025-10-02 01:13:21 -0600

    style: function comment

--- extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes.lua
+++ extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes.lua
@@ -140,7 +140,15 @@ local function copy_attachments_to_siblings(first_mark_id)
     end
 end
 
-local function create_alignment_for_sights(table_to_insert_into, vectors_table)
+-- ######
+-- Create Alignments for Sights
+-- DESCRIPTION: generates sight offsets with given position/rotation
+-- PARAMETERS: 
+--  table_to_insert_into: table; the fixes table
+--  vectors_table: table of string, vector3_box; what the vector is used for and the vector value
+-- RETURN: N/A
+-- ######
+local function create_alignments_for_sights(table_to_insert_into, vectors_table)
     table_insert(table_to_insert_into, {
             attachment_slot = "sight_offset",
             requirements = {
@@ -428,45 +436,45 @@ extended_weapon_customization_plugin.fixes.autogun_p3_m1 = {
     },
 }
 ]]
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.autopistol_p1_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.autopistol_p1_m1, {
     position = vector3_box(-0.045, -0.05, 0.1175),
     rotation = vector3_box(-2, 10, -2),
 })
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.bolter_p1_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.bolter_p1_m1, {
     position = vector3_box(0, 0, -0.0095),
 })
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.boltpistol_p1_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.boltpistol_p1_m1, {
     position = vector3_box(0, 0, -0.0095),
 })
 -- Infantry Lasguns don't need alignment
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.lasgun_p2_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.lasgun_p2_m1, {
     position_for_1 = vector3_box(0, 0, -0.0235),
     position_for_2 = vector3_box(0, 0, -0.022),
     position_for_3 = vector3_box(0, 0, -0.022),
 })
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.lasgun_p3_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.lasgun_p3_m1, {
     position = vector3_box(0, 0.15, -0.0235),
 })
 -- Laspistols don't need alignment
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.ogryn_heavystubber_p1_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.ogryn_heavystubber_p1_m1, {
     position = vector3_box(-0.28, -0.5, 0.18),
     rotation = vector3_box(-2.4, 0, -1.4),
 })
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.ogryn_heavystubber_p2_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.ogryn_heavystubber_p2_m1, {
     position = vector3_box(0.06, -0.25, 0.25),
     rotation = vector3_box(-3.1, 0, -2.2),
 })
 -- Combat Shotguns don't need alignment
 --[[
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.shotgun_p1_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.shotgun_p1_m1, {
     position = vector3_box(0, 0, -0.0335),
 })
 ]]
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.shotgun_p4_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.shotgun_p4_m1, {
     position = vector3_box(-0.09, 0, 0.13),
     rotation = vector3_box(-6, 0, -5.5),
 })
-create_alignment_for_sights(extended_weapon_customization_plugin.fixes.stubrevolver_p1_m1, {
+create_alignments_for_sights(extended_weapon_customization_plugin.fixes.stubrevolver_p1_m1, {
     position = vector3_box(0.00, 0, -0.02),
 })
 
