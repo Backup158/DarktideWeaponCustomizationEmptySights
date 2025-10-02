334534c050089bdeeae8cb1e8de4553808612abe
LackupL58 <beekoop158+gh@gmail.com>
2025-10-02 01:07:34 -0600

    local: mod name

--- extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes_data.lua
+++ extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("extended_weapon_customization_empty_scopes")
 
 return {
-	name = "extended_weapon_customization_empty_scopes",
+	name = "EWC - Empty Scopes",
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 	options = {
