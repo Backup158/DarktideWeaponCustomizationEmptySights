90c6fa9687f0a03ecd4abbbffd8d7b31da0da3b6
LackupL58 <beekoop158+gh@gmail.com>
2025-10-03 01:22:43 -0600

    fix: attachment slot local code

--- extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes_localization.lua
+++ extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes_localization.lua
@@ -7,7 +7,7 @@ mod:add_global_localize_strings({
         en = "Empty Scopes/Sights",
     },
 	-- Slot Name
-	loc_sight_reticles = {
+	attachment_slot_sight_reticles = {
 		en = "Sight Reticles",
 	},
 })
