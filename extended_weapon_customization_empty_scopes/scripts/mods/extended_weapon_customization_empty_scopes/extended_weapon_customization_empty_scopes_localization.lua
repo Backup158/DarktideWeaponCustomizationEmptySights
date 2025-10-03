d384082f28548ee24e68612a875167d57a45001b
LackupL58 <beekoop158+gh@gmail.com>
2025-10-03 09:17:17 -0600

    local: new name code for groups

--- extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes_localization.lua
+++ extended_weapon_customization_empty_scopes/scripts/mods/extended_weapon_customization_empty_scopes/extended_weapon_customization_empty_scopes_localization.lua
@@ -3,7 +3,7 @@ local mod = get_mod("extended_weapon_customization_empty_scopes")
 -- Localizations for the base mod to use
 mod:add_global_localize_strings({
 	-- Mod Name for the separators
-    loc_extended_weapon_customization_empty_scopes = {
+    loc_ewc_extended_weapon_customization_empty_scopes = {
         en = "Empty Scopes/Sights",
     },
 	-- Slot Name
